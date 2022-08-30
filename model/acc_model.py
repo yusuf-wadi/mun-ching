45e913df91f982cb5e006efef2e0df09e02ccf63
Yusuf Wadi <yusufwadi1@gmail.com>
2022-08-30 15:46:52 -0500

    adding mem

--- model/acc_model.py
+++ model/acc_model.py
@@ -7,13 +7,18 @@ cur_model = "davinci:ft-valley-2022-08-28-00-10-42"
 
 curcon_path = 'model/text/mem/cur_convo.txt'
 res_path = 'model/text/response.txt'
+ctx_path = 'model/text/mem/cntx_prompt.txt'
 
 cur_convo = open(curcon_path,"a")
+
+with open(ctx_path, "w+") as ctx:
+    ctxPrompt = ctx.read()
+
 
 with open(res_path, "w") as f:
     completion = openai.Completion.create(
         model= cur_model,
-        prompt= '"' + YOUR_PROMPT + '"',
+        prompt= ctxPrompt,
         temperature = 0.3,
         presence_penalty = 0.5,
         frequency_penalty = 0.5,
